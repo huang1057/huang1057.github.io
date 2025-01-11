c8292ed8b4da43804badeaf17b26da6b66e821cb
huang1057 <huang1057@hotmail.com>
2025-01-11 21:20:09 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'users';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // 设置 PDO 错误模式为异常
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
